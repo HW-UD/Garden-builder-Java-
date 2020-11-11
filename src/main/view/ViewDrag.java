ff3053691e0ba4877246372b265e7a0aed6f4e28
Huawei Wang <whw@udel.edu>
2020-11-11 02:01:01 -0500

    ViewDrag part down

--- src/main/view/ViewDrag.java
+++ src/main/view/ViewDrag.java
@@ -1,14 +1,23 @@
 package view;
+import java.awt.MouseInfo;
+import java.awt.Point;
+
 import controller.DragController;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
@@ -25,52 +34,96 @@ public class ViewDrag extends ViewBase {
 //		super(stage, handlerP, handlerN);
 //		// TODO Auto-generated constructor stub
 //	}
-	private ImageView iv1;
+	private ImageView iv1 = new ImageView();/////////要改
 	private final int imgwidth = 100;
 	private final int imgheight = 100;
 	
+	@Override
 	public Scene getScene() {
-		BorderPane borderpane = new BorderPane();
+		BorderPane root = new BorderPane();
 		
-//    	TilePane tilepane = new TilePane();
-//        FlowPane flowpane = new FlowPane();
-//        tilepane.setStyle("-fx-background-color : #7CFC00;");
-//        flowpane.setStyle("-fx-background-color : #8B4513;");
-//    	HBox hbox = new HBox();
-//    	VBox vbox = new VBox();
-//    	borderpane.setTop(hbox);
-//    	borderpane.setRight(vbox);
-//    	borderpane.setCenter(flowpane);
-//    	borderpane.setLeft(tilepane);
-//    	
-// 
-//    	Image im1 = new Image(getClass().getResourceAsStream("commonMilkweed.png"));
-//    	iv1.setImage(im1);
-//    	tilepane.getChildren().add(iv1);
-//    	iv1.setPreserveRatio(true);
-//    	iv1.setFitHeight(imgheight);
-//    	iv1.setFitWidth(imgwidth);
+    	TilePane tilepane = new TilePane();
+        FlowPane flowpane = new FlowPane();
+        tilepane.setStyle("-fx-background-color : #7CFC00;");
+        flowpane.setStyle("-fx-background-color : #8B4513;");
+    	HBox hbox = new HBox();
+    	VBox vbox = new VBox();
+    	root.setTop(hbox);
+    	root.setRight(vbox);
+    	root.setCenter(flowpane);
+    	root.setLeft(tilepane);
+    	
+ 
+    	Image im1 = new Image(getClass().getResourceAsStream("commonMilkweed.png"));
+    	iv1.setImage(im1);
+    	tilepane.getChildren().add(iv1);
+    	iv1.setPreserveRatio(true);
+    	iv1.setFitHeight(imgheight);
+    	iv1.setFitWidth(imgwidth);
 		
-	   BorderPane root = new BorderPane();
 		
-		root.setPadding(new Insets(10));
-		Label label = new Label("drag");
-		label.setFont(new Font(32));
-		root.setCenter(label);
+		
+//		root.setPadding(new Insets(10));
+//		Label label = new Label("Drag");
+//		label.setFont(new Font(32));
+//		root.setCenter(label);
 		
 		Button backButton = new Button("Previous");
 		backButton.setOnMousePressed(handlerP);
 		Button nextButton = new Button("Next");
 		nextButton.setOnMousePressed(handlerN);
-//		Button closeButton = new Button("Close");
-//		closeButton.setOnMousePressed(e -> stage.close());
+
 		
 		ButtonBar bbar = new ButtonBar();
 		bbar.setPadding(new Insets(10, 0, 0, 10));
 		bbar.getButtons().addAll(backButton, nextButton);
-		borderpane.setBottom(bbar);
+		root.setBottom(bbar);
 
-		return new Scene(borderpane, WIDTH, HEIGHT);
+		iv1.setOnDragDetected(new EventHandler<MouseEvent>(){
+    		@Override
+    		public void handle(MouseEvent event){
+    			Dragboard iv2 = iv1.startDragAndDrop(TransferMode.COPY);
+    			ClipboardContent content = new ClipboardContent();
+    			content.putImage(iv1.getImage());
+    			iv2.setContent(content);
+    			WritableImage wi = new WritableImage(imgwidth,imgheight);
+    			iv1.snapshot(new SnapshotParameters(), wi);
+    			iv2.setDragView(wi);   		
+
+    		}
+    	});
+    	
+    	flowpane.setOnDragDropped(new EventHandler<DragEvent>() {
+    		@Override
+    		public void handle(DragEvent event) {
+    			ImageView iv1copy = new ImageView(event.getDragboard().getImage());
+    			iv1copy.setPreserveRatio(true);
+    			iv1copy.setFitHeight(imgheight);
+    			iv1copy.setFitWidth(imgwidth);
+    			
+
+    			flowpane.getChildren().add(iv1copy);
+    			
+//    			Point p = MouseInfo.getPointerInfo().getLocation();
+//
+//    			iv1copy.setTranslateX(p.getX()- 550);
+//    			iv1copy.setTranslateY(p.getY());//
+    			
+    			iv1copy.setTranslateX(event.getX());
+    			iv1copy.setTranslateY(event.getY());
+    		}
+    	});
+    	
+    	flowpane.setOnDragOver(new EventHandler<DragEvent>() {
+    		@Override
+    		public void handle(DragEvent event) {
+    			event.acceptTransferModes(TransferMode.COPY);
+    		}
+    	});
+		
+		
+		
+		return new Scene(root, WIDTH, HEIGHT);
 	}
 	
 	
