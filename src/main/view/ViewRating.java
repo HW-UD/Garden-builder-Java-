ae9563218d66a83860dd5e5e837c3cb6c92796ae
Yuzu_Wu <wuyuzu@udel.edu>
2020-11-12 08:06:35 -0500

    fix rating

--- src/main/view/ViewRating.java
+++ src/main/view/ViewRating.java
@@ -1,9 +1,10 @@
 package view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javax.swing.event.ChangeListener;
 import controller.RatingController;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -15,8 +16,7 @@ import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
-
+import java.util.ArrayList;
 
 
 public class ViewRating implements ViewMaker {
@@ -34,12 +34,29 @@ public class ViewRating implements ViewMaker {
 		
 		BorderPane root = new BorderPane();
 
-		
+
+		ArrayList<ImageView> arrayList = new ArrayList<>();
+		for(int i = 1;i<6;i++){
+			ImageView imageView = new ImageView(new Image("file:src/main/img/rating/"+i+"_star.png "));
+			imageView.setFitWidth(230);
+			imageView.setFitHeight(50);
+			arrayList.add(imageView);
+			}
+
 		Text label = new Text("Please rate for us:\nChose 1~5");
 		label.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
-		root.setTop(label);
-		ComboBox ScoreComboBox = new ComboBox();
+		
+		ComboBox<String>  ScoreComboBox = new ComboBox<String>();
         ScoreComboBox.getItems().addAll("5","4","3","2","1");
+        
+        //get the value in combobox
+		ScoreComboBox.valueProperty().addListener(new ChangeListener<String>() {
+			@Override 
+			public void changed(ObservableValue ov, String t, String t1) {
+				root.setCenter(arrayList.get(Integer.parseInt(t1)-1));
+			}
+		});
+		root.setTop(label);
 		
 		Text category = new Text("Score:");
 		category.setFont(Font.font("Times New Roman", FontWeight.BOLD, 20));
@@ -48,7 +65,7 @@ public class ViewRating implements ViewMaker {
         grid.add(ScoreComboBox, 0, 1);
 		grid.setVgap(20);
 		grid.setHgap(100);
-		grid.setPadding((new Insets(1,0,0,1)));
+		grid.setPadding((new Insets(1,6,6,1)));
 		root.setLeft(grid);
 		
 		
@@ -57,23 +74,13 @@ public class ViewRating implements ViewMaker {
 		Button closeButton = new Button("Close");
 		closeButton.setOnMousePressed(e -> stage.close());
 		ButtonBar bbar = new ButtonBar();
-		bbar.setPadding(new Insets(100));
+		bbar.setPadding(new Insets(101));
 		bbar.getButtons().addAll(backButton, closeButton);
 		root.setBottom(bbar);
-		bbar.setPadding(new Insets(10, 5, 5, 10));
+		bbar.setPadding(new Insets(10, 4, 4, 10));
 		BorderPane.setMargin(grid, new Insets(100,0,0,200));
 		
-		//  detect the number in Combobox.
 	    System.out.println(ScoreComboBox.getEditor().getText());
-	    Image img =  new Image(getClass().getResourceAsStream("../img/rating/5_star.png"));			
-		ImageView iv1 = new ImageView();
-		iv1.setImage(img);
-		root.getChildren().add(iv1);
-		iv1.setPreserveRatio(true);
-		iv1.setFitWidth(350);
-	    iv1.setTranslateX(iv1.getTranslateX()+325);
-	    iv1.setTranslateY(iv1.getTranslateY()+170);
-
 		Scene scene = new Scene(root, 1000, 600);
 		return scene;
 	}
