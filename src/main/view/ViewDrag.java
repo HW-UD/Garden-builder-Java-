248c44fc14e71e9880de57eea2c0ee61c19ce7da
RuihengX <xierh@udel.edu>
2020-12-01 03:19:34 -0500

    edit viewdrag

--- src/main/view/ViewDrag.java
+++ src/main/view/ViewDrag.java
@@ -121,9 +121,10 @@ public class ViewDrag extends ViewBase {
 //    	glareplants.add(new Image("file:src/main/img/default/tudi.jpg"));
 //    	
     	sbutton.setOnAction(e -> {
-    		loadFile("/Users/ruiheng/eclipse-workspace/project-team-14/src/main/img/light");
-        	for (Image i: plants_img) {
-        		ImageView iv1 = new ImageView();
+    		loadFile("/Users/ruiheng/eclipse-workspace/project-team-14/src/main/img/spring");
+        	for (GardenImage i: plants_img) {
+        		GardenImgView iv1 = new GardenImgView();
+        		iv1.setID(i.getID());
             	iv1.setImage(i);
             	tilepane.getChildren().add(iv1);
             	iv1.setPreserveRatio(true);
@@ -133,18 +134,18 @@ public class ViewDrag extends ViewBase {
         	}
         	});
         
-        sbutton.setOnAction(e -> {
-    		loadFile("/Users/ruiheng/eclipse-workspace/project-team-14/src/main/img/water");
-        	for (Image iv: plants_img) {
-        		ImageView iv2 = new ImageView();
-            	iv2.setImage(iv);
-            	tilepane.getChildren().add(iv2);
-            	iv2.setPreserveRatio(true);
-            	iv2.setFitHeight(imgheight);
-            	iv2.setFitWidth(imgwidth);
-            	DragController.drag (iv2);
-        	}
-		});
+//        sbutton.setOnAction(e -> {
+//    		loadFile("/Users/ruiheng/eclipse-workspace/project-team-14/src/main/img/spring");
+//        	for (Image iv: plants_img) {
+//        		GardenImgView iv2 = new GardenImgView();
+//            	iv2.setImage(iv);
+//            	tilepane.getChildren().add(iv2);
+//            	iv2.setPreserveRatio(true);
+//            	iv2.setFitHeight(imgheight);
+//            	iv2.setFitWidth(imgwidth);
+//            	DragController.drag (iv2);
+//        	}
+//		});
         
         
         ScrollPane scrollPane = new ScrollPane(tilepane);
